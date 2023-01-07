23ac3b5295358dfe88ae54bd400a9a4cb2fadf1c
destiny3912 <destiny3910@gmail.com>
2023-01-06 17:22:29 -0800

    project init

---
+++ DeepFX/src/main/java/com/deepfx/serviceserver/DeepFXApplication.java
@@ -0,0 +1,13 @@
+package com.deepfx.serviceserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeepFXApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeepFXApplication.class, args);
+	}
+
+}
---
+++ DeepFX/src/test/java/com/deepfx/serviceserver/ServiceserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deepfx.serviceserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
